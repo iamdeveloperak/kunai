15570f7131a34af1d4149c8da5b12d608230e4d8
Akash Singh <akxssj38@gmail.com>
2021-06-24 04:28:26 +0530

    models migrated

--- website/admin.py
+++ website/admin.py
@@ -4,12 +4,11 @@ from .models import SiteMeta, Page, PageSection, InfoCard
 # admin.site.register(SiteMeta)
 @admin.register(SiteMeta)
 class SiteMetaAdmin(admin.ModelAdmin):
-    # def has_add_permission(self, request):
-    #     return False
+    def has_add_permission(self, request):
+        return True
 
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
-    pass
+    def has_delete_permission(self, request, obj=None):
+        return True
 
 admin.site.register(Page)
 
