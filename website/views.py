e9965f931b9ff71092e49dfbf8162f540256358e
Akash Singh <akxssj38@gmail.com>
2021-06-24 01:44:16 +0530

    website/views.py

--- website/views.py
+++ website/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from .models import SiteMeta, Page, PageSection, InfoCard
 from django.contrib.auth.decorators import login_required
 # Create your views here.
-sitedata = SiteMeta.objects.first()
+# sitedata = SiteMeta.objects.first()
 def index(request):
+    sitedata = SiteMeta.objects.first()
     pagedata = Page.objects.filter(page_name="index").first()
     sectiondata = PageSection.objects.get(title__icontains="amazing features")
     data = {
@@ -21,18 +22,21 @@ def tracking_list(request):
     return render(request, 'tracking-list.html', data)
 
 def privacyterms(request):
+    sitedata = SiteMeta.objects.first()
     context = {
         'sitedata': sitedata, 
     }
     return render(request, 'terms/privacyterms.html', context)
 
 def privacypolicy(request):
+    sitedata = SiteMeta.objects.first()
     context = {
         'sitedata': sitedata, 
     }
     return render(request, 'terms/privacypolicy.html', context)
 
 def termsandconditions(request):
+    sitedata = SiteMeta.objects.first()
     context = {
         'sitedata': sitedata, 
     }
