858c0f408258839acf22a9628ce41ef930684dd7
Akash Singh <akxssj38@gmail.com>
2021-06-24 04:53:04 +0530

    models migrated

--- kunai/settings.py
+++ kunai/settings.py
@@ -91,14 +91,14 @@ WSGI_APPLICATION = 'kunai.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'kunai',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'kunai',
+    #     'USER': 'root',
+    #     'PASSWORD': 'root',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # },
     # 'postgres': {
     #     'ENGINE': 'django.db.backends.postgres',
     #     'NAME': 'kunai',
@@ -107,14 +107,14 @@ DATABASES = {
     #     'HOST': 'ec2-34-232-191-133.compute-1.amazonaws.com',
     #     'PORT': '5432',
     # },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'kunai',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'Akashdecristiano7',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432',
-    # },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kunai',
+        'USER': 'postgres',
+        'PASSWORD': 'Akashdecristiano7',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
